ae19906ffb8c66dc4beb371bfb80b14504d2a3ac
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-12-29 19:55:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ const selectDropdownOption = async (page, groupIndex, itemIndex) => {
 
 const cities = [1, 5, 6, 7];
 
-const brokerTypes = [1, 2];
-
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -31,77 +29,72 @@ const brokerTypes = [1, 2];
   await page.waitForSelector(".block-from-group");
 
   for (const city of cities) {
-    for (const brokerType of brokerTypes) {
-      await selectDropdownOption(page, 1, 1);
-      await selectDropdownOption(page, 2, brokerType);
-      await selectDropdownOption(page, 4, city);
+    await selectDropdownOption(page, 1, 1);
+    await selectDropdownOption(page, 2, 2);
+    await selectDropdownOption(page, 4, city);
 
-      console.log("Clicking search button.");
+    console.log("Clicking search button.");
 
-      await page.waitForNetworkIdle();
-      await page.click(".sc-knesRu.JDvEH.dgaui.dgaui_button.buttonMw160");
-      await page.waitForNetworkIdle();
-      await page.waitForSelector(".sc-dJDBYC.ecwOXH.dgaui.dgaui_pagination");
+    await page.waitForNetworkIdle();
+    await page.click(".sc-knesRu.JDvEH.dgaui.dgaui_button.buttonMw160");
+    await page.waitForNetworkIdle();
+    await page.waitForSelector(".sc-dJDBYC.ecwOXH.dgaui.dgaui_pagination");
 
-      console.log("Search button clicked, waiting for network idle.");
+    console.log("Search button clicked, waiting for network idle.");
 
-      while (true) {
+    while (true) {
+      try {
+        await waitFor(1000);
+
+        const links = await page.$$eval(".blockForm.clickable", (links) =>
+          links.map(
+            (link) =>
+              "https://eservicesredp.rega.gov.sa" + link.getAttribute("href")
+          )
+        );
+
+        console.log("Links found:", links);
+
         try {
-          await waitFor(1000);
-
-          const links = await page.$$eval(".blockForm.clickable", (links) =>
-            links.map(
-              (link) =>
-                "https://eservicesredp.rega.gov.sa" + link.getAttribute("href")
-            )
+          const existingData = JSON.parse(
+            await fs.readFile("newLinks.json", "utf8")
           );
-
-          console.log("Links found:", links);
-
-          try {
-            const existingData = JSON.parse(
-              await fs.readFile("newLinks.json", "utf8")
-            );
-            const updatedData = [...existingData, ...links];
-            await fs.writeFile(
-              "newLinks.json",
-              JSON.stringify(updatedData, null, 2)
-            );
-          } catch (error) {
-            if (error.code === "ENOENT") {
-              await fs.writeFile(
-                "newLinks.json",
-                JSON.stringify(links, null, 2)
-              );
-            } else {
-              throw error;
-            }
-          }
-
-          const nextButton = await page.$$(
-            ".sc-irEpRR.hjEfEt.dgaui.dgaui_tab.noAfter"
+          const updatedData = [...existingData, ...links];
+          await fs.writeFile(
+            "newLinks.json",
+            JSON.stringify(updatedData, null, 2)
           );
-
-          if (nextButton.length === 0) {
-            console.log("Next button not found, exiting loop.");
-            break;
+        } catch (error) {
+          if (error.code === "ENOENT") {
+            await fs.writeFile("newLinks.json", JSON.stringify(links, null, 2));
+          } else {
+            throw error;
           }
+        }
 
-          const lastNextButton = nextButton[nextButton.length - 1];
+        const nextButton = await page.$$(
+          ".sc-irEpRR.hjEfEt.dgaui.dgaui_tab.noAfter"
+        );
 
-          if (!lastNextButton) {
-            console.log("Last next button not found, exiting loop.");
-            break;
-          }
-
-          console.log("Clicking next button.");
-
-          await lastNextButton.click();
-          await page.waitForNetworkIdle();
-        } catch (e) {
-          console.log("Error: ", e);
+        if (nextButton.length === 0) {
+          console.log("Next button not found, exiting loop.");
           break;
         }
+
+        const lastNextButton = nextButton[nextButton.length - 1];
+
+        if (!lastNextButton) {
+          console.log("Last next button not found, exiting loop.");
+          break;
+        }
+
+        console.log("Clicking next button.");
+
+        await lastNextButton.click();
+        await page.waitForNetworkIdle();
+      } catch (e) {
+        console.log("Error: ", e);
+        break;
       }
     }
   }
